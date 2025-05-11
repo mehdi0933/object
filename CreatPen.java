bc480dbfab72ef020e21188e77e2e1e57cab3b58
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-05-11 23:44:11 +0200

    Add files via upload

---
+++ CreatPen.java
@@ -0,0 +1,17 @@
+package javacore.chapiter05.object.exercise;
+
+public class CreatPen {
+     public static void main(String[] args) {
+
+          Pen pen1 = new Pen();
+          pen1.nom = "Stylo";
+          pen1.couleur = "bleu";
+          pen1.frabicant = "Bic";
+          pen1.prix = 2.50;
+          pen1.quantite = 10;
+          pen1.poids = 15;
+          pen1.displayInformation();
+     }
+}
+
+
---
+++ Pen.java
@@ -0,0 +1,16 @@
+package javacore.chapiter05.object.exercise;
+
+public class Pen {
+
+        String nom;
+        String couleur;
+        String frabicant;
+        double prix;
+        int quantite;
+        int poids;
+
+    public void displayInformation(){
+        System.out.println("Nom: " + nom + "| Couleur: " + couleur +" | Prix: " + prix + "€" + " | Quantité:" + quantite + " | Fabricant:" + frabicant + " | Poids: " + poids);
+
+    }
+}
