579cf13f97c0d0cdb2df709dd55609812a0bb577
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-05-16 15:36:08 +0200

    Add files via upload

---
+++ CarImprovement.java
@@ -0,0 +1,7 @@
+public class CarImprovement {
+    public static void main(String[] args) {
+
+        PracticeObjectCar car1 = new PracticeObjectCar("AZ345FB", "porsche", "carreta GT", " jaune", 1000000, 250000 );
+        car1.started(true);
+    }
+}
---
+++ PracticeObjectCar.java
@@ -0,0 +1,43 @@
+public class PracticeObjectCar {
+
+    String serialNumero;
+    String brand;
+    String model;
+    String color;
+
+    double price;
+    double odometre;
+
+    boolean isEngineStarted;
+
+    public PracticeObjectCar(String serialNumero, String brand, String model, String color, double price, double odometre){
+        this.serialNumero = serialNumero;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+        this.odometre = 0;
+        this.isEngineStarted = false;
+
+        this.displayInformation();
+    }
+
+    public double run(double distance){
+        System.out.println("la voiture roule pendans " +distance+ " kms.");
+        odometre += distance;
+        return distance;
+    }
+
+    public void displayInformation(){
+        System.out.println("serialNumero" + serialNumero + "| model " + model + " | color " + color + " | price " + price + " | odometre " + odometre);
+    }
+
+    public void started(boolean isEngineStarted){
+        if(isEngineStarted == isEngineStarted){
+            System.out.println("le moteur ne tourne pas");
+        }{
+            System.out.println("le moteur tourne");
+        }
+    }
+
+}
