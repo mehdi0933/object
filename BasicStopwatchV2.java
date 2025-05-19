79577c6c71a837270cfb6f4e415d19a47ceb316d
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-05-19 19:09:56 +0200

    Add files via upload

--- BasicStopwatchV2.java
+++ BasicStopwatchV2.java
@@ -1,6 +1,6 @@
 public class BasicStopwatchV2{
 
-    int secoond;
+    int seconde;
     int minute;
     int hours;
     boolean isChrno;
@@ -10,12 +10,12 @@ public class BasicStopwatchV2{
         while (isChrno) {
 
             Thread.sleep(1000);
-            secoond++;
+            seconde++;
 
 
-            if (secoond >= 59) {
+            if (seconde >= 59) {
 
-                secoond = 0;
+                seconde = 0;
                 minute++;
 
             }
@@ -23,24 +23,64 @@ public class BasicStopwatchV2{
             if (minute >= 60) {
 
                 minute = 0;
-                secoond++;
+                seconde++;
 
             }
 
             if (hours >= 24) {
                 hours = 0;
             }
-
-            String displayHours = hours < 10 ? "0" + hours : "" + hours;
-            String displayMinute = minute < 10 ? "0" + minute : "" + minute;
-            String displaySecoond = secoond < 10 ? "0" + secoond : "" + secoond;
-
-            System.out.println(displayHours + ":" + displayMinute + ":" + displaySecoond);
-
-            // System.out.printf("%02d:%02d:%02d%n", hours , minute, secoond);
-
-
         }
+        
+    }
+
+    public void displayChrono(){
+        String displayHours   = this.hours < 10 ? "0" + this.hours : "" + this.hours;
+        String displayMinute  = this.minute < 10 ? "0" + this.minute : "" + this.minute;
+        String displaySecoond = this.seconde < 10 ? "0" + this.seconde : "" + this.seconde;
+
+        System.out.println(displayHours + ":" + displayMinute + ":" + displaySecoond);
+
+        // System.out.printf("%02d:%02d:%02d%n", hours , minute, seconde);
+    }
+
+    public void cookHardBoiledEggs() throws InterruptedException  {
+
+        while (isChrno) {
+
+            Thread.sleep(1000);
+            seconde++;
+
+
+            if (seconde >= 59) {
+
+                seconde = 0;
+                minute++;
+
+            }
+
+            if (minute >= 60) {
+
+                minute = 0;
+                seconde++;
+
+            }
+
+            if (hours >= 24) {
+                hours = 0;
+            }
+        }
+
+    }
+
+    public void displayChronoCookHardBoiledEggs(){
+        String displayHours   = this.hours < 10 ? "0" + this.hours : "" + this.hours;
+        String displayMinute  = this.minute < 10 ? "0" + this.minute : "" + this.minute;
+        String displaySecoond = this.seconde < 10 ? "0" + this.seconde : "" + this.seconde;
+
+        System.out.println("Œufs Durs : " displayHours + ":" + displayMinute + ":" + displaySecoond);
+
+        // System.out.printf("%02d:%02d:%02d%n", hours , minute, seconde);
     }
 }
 
