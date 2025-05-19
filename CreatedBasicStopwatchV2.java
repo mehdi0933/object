04e3b8358878610f39ce4611a7851eaa4b7a9107
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-05-19 19:16:41 +0200

    Add files via upload

--- CreatedBasicStopwatchV2.java
+++ CreatedBasicStopwatchV2.java
@@ -1,4 +1,4 @@
-public class createdBasicStopwatchV2 {
+public class CreatedBasicStopwatchV2 {
     public static void main(String[] args) throws InterruptedException {
         BasicStopwatchV2  whatch = new BasicStopwatchV2();
         whatch.seconde  = 0;
