72c8ccedc5beb56ab4747c137f26d3339eadd152
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-05-19 18:47:52 +0200

    Add files via upload

--- CarImprovement.java
+++ CarImprovement.java
@@ -1,7 +1,13 @@
 public class CarImprovement {
     public static void main(String[] args) {
 
-        PracticeObjectCar car1 = new PracticeObjectCar("AZ345FB", "porsche", "carreta GT", " jaune", 1000000, 250000 );
-        car1.started(true);
+        PracticeObjectCar car1 = new PracticeObjectCar("AZ345FB", "porsche", "carrera GT", " jaune", 1000000, 250000 );
+        car1.isEngineStarted = true;
+        car1.run(1000);
+        car1.run(2);
+        car1.started();
+        car1.isEngineStarted = false;
+        car1.run(200);
+        car1.displayInformation();
     }
 }
