102cb2255a6ba7b0fd5a5970e0c7a60e936553c0
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-05-27 20:46:22 +0200

    Add files via upload

---
+++ ReversedString.java
@@ -0,0 +1,15 @@
+public class ReversedString {
+    public static void main(String[] args) {
+
+        String text = "! ecicrexe'l issuér zeva suov ,ovarB";
+        int textLength = text.length();
+        // Ecrivez le code ci-dessous
+
+        for (int i = 0; i < text.length(); i++  ){
+            System.out.print(text.charAt(textLength -i -1));
+
+
+        }
+
+    }
+}
