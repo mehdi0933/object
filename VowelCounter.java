6bc8bd58166443b715299e59a8880394022ea477
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-05-29 19:58:10 +0200

    Add files via upload

--- VowelCounter.java
+++ VowelCounter.java
@@ -1,51 +1,24 @@
 public class VowelCounter {
     public static void main(String[] args) {
 
-        String sentenceLoremIpsum ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        String  sentenceLoremIpsumAllLowerCase = sentenceLoremIpsum.toLowerCase();
+        String sentenceLoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        String sentenceLoremIpsumAllLowerCase = sentenceLoremIpsum.toLowerCase();
 
-        char letterA = 'a';
-        char letterE = 'e';
-        char letterI = 'i';
-        char letterO = 'o';
-        char letterU = 'u';
-        char letterY = 'y';
-
-        int counterA = 0;
-        int counterE = 0;
-        int counterI = 0;
-        int counterO = 0;
-        int counterU = 0;
-        int counterY = 0;
-
+        int[] countLetter = {0, 0, 0, 0, 0, 0};
+        char[] Letter = {'a', 'e', 'i', 'o', 'u', 'y'};
 
         for (int index = 0; index < sentenceLoremIpsumAllLowerCase.length(); index++) {
-            if(sentenceLoremIpsumAllLowerCase.charAt(index) == letterA){
-                counterA++;
+            for (int analysisLetter = 0; analysisLetter < Letter.length; analysisLetter++) {
+                if (sentenceLoremIpsumAllLowerCase.charAt(index) == Letter[analysisLetter]) {
+                    countLetter[analysisLetter]++;
+                }
             }
-            if(sentenceLoremIpsumAllLowerCase.charAt(index) == letterE){
-                counterE++;
-            }
-            if(sentenceLoremIpsumAllLowerCase.charAt(index) == letterI){
-                counterI++;
-            }
-            if(sentenceLoremIpsumAllLowerCase.charAt(index) == letterO){
-                counterO++;
-            }
-            if(sentenceLoremIpsumAllLowerCase.charAt(index) == letterU){
-                counterU++;
-            }
-            if(sentenceLoremIpsumAllLowerCase.charAt(index) == letterY){
-                counterY++;
-            }
-
         }
-        System.out.println("Il y a " + counterA + " fois la lettre A.");
-        System.out.println("Il y a " + counterE + " fois la lettre E.");
-        System.out.println("Il y a " + counterI + " fois la lettre I.");
-        System.out.println("Il y a " + counterO + " fois la lettre O.");
-        System.out.println("Il y a " + counterU + " fois la lettre U.");
-        System.out.println("Il y a " + counterY + " fois la lettre Y.");
+
+// Affichage des résultats
+        for (int i = 0; i < Letter.length; i++) {
+            System.out.println("Il y a "+ countLetter[i] + " fois la lettre " +  Letter[i]+".");
+        }
     }
 }
 /**
