6f972ea59b4a6add92b3eac8e91c90c5d8d4490a
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-06-09 23:27:32 +0200

    Add files via upload

--- SubstitutionCipher.java
+++ SubstitutionCipher.java
@@ -1,48 +1,82 @@
 public class SubstitutionCipher {
     public static void main(String[] args) {
+
         /**
          * Alphabet Standard (Latin)
          */
         String latinAlphabet = "abcdefghijklmnopqrstuvwxyz";
 
         /**
-         *  Alphabet de Substitution
+         * Alphabet de Substitution
          */
         String substitutionAlphabet = "ntrhwbgeyjzlsoaqdmcuvfpxik";
-
+        int iterations = 19;
         /**
-         *  Texte à chiffrer
+         * Texte à chiffrer
          */
         String textToEncrypt = "ce message secret ne doit pas arriver entre de mauvaises mains !";
-        boolean isEspece = true;
-        boolean isendOfMessage = true;
-        // Ecrivez le code ci-dessous
+        String textEncrypt = cipher(textToEncrypt, latinAlphabet, substitutionAlphabet, iterations);
+        System.out.println("Texte chiffré   : " + textEncrypt);
 
-        for (int i = 0; i < textToEncrypt.length(); i++){
+        String decrypted = decipher(textEncrypt, latinAlphabet, substitutionAlphabet, iterations);
+        System.out.println("Texte déchiffré : " + decrypted );
 
-            for (int j = 0; j < latinAlphabet.length(); j++){
-                char espace = ' ';
-                char changeLetter = ' ';
-                char exclamationMark ='!';
+    }
 
-                if(textToEncrypt.charAt(i)  == latinAlphabet.charAt(j) ) {
-                    changeLetter = textToEncrypt.charAt(i);
-                    changeLetter = substitutionAlphabet.charAt(j) ;
-                    System.out.print(changeLetter);
-                    isEspece = true;
-                }else if (textToEncrypt.charAt(i) == espace && isEspece ){
-                    System.out.print(textToEncrypt.charAt(i));
-                    isEspece = false;
-                }else if (textToEncrypt.charAt(i) == exclamationMark && isendOfMessage){
-                    System.out.print(exclamationMark);
-                    isendOfMessage = false;
+    public static String cipher(String textToEncrypt, String latinAlphabet, String substitutionAlphabet, int iterations) {
+        for (int J = 0; J < iterations; J++) {
+            String currentEncryptedText = "";
+
+            for (int i = 0; i < textToEncrypt.length(); i++) {
+                if (latinAlphabet.indexOf(textToEncrypt.charAt(i)) != -1) {
+                    currentEncryptedText = currentEncryptedText + substitutionAlphabet.charAt(latinAlphabet.indexOf(textToEncrypt.charAt(i)));
+                } else {
+                    currentEncryptedText = currentEncryptedText + textToEncrypt.charAt(i);
                 }
-
             }
+
+            textToEncrypt = currentEncryptedText;
+
         }
+
+        return textToEncrypt;
+    }
+
+    public static String decipher(String textEncrypt, String latinAlphabet, String substitutionAlphabet, int iterations) {
+        for (int J = 0; J < iterations; J++) {
+            String currenDecryptedText = "";
+
+            for (int i = 0; i < textEncrypt.length(); i++) {
+                if (substitutionAlphabet.indexOf(textEncrypt.charAt(i)) != -1) {
+                    currenDecryptedText = currenDecryptedText + latinAlphabet.charAt(substitutionAlphabet.indexOf(textEncrypt.charAt(i)));
+                } else {
+                    currenDecryptedText = currenDecryptedText + textEncrypt.charAt(i);
+                }
+            }
+
+            textEncrypt = currenDecryptedText;
+
+        }
+
+        return textEncrypt;
     }
 }
-/*
-    resustat de la console
-    rw swccngw cwrmwu ow hayu qnc nmmyfwm woumw hw snvfnycwc snyoc !
+
+
+/**
+ *     for (int J = 0; J < iterations; J++) {
+ *         String currentEncryptedText = "";
+ *
+ *         for (int i = 0; i < textToEncrypt.length(); i++) {
+ *             if (latinAlphabet.indexOf(textToEncrypt.charAt(i)) != -1) {
+ *                 currentEncryptedText = currentEncryptedText + substitutionAlphabet.charAt(
+ *                     latinAlphabet.indexOf(textToEncrypt.charAt(i))
+ *                 );
+ *             } else {
+ *                 currentEncryptedText = currentEncryptedText + textToEncrypt.charAt(i);
+ *             }
+ *         }
+ *         textToEncrypt = currentEncryptedText;
+ *     }
+ *     return textToEncrypt;
  */
